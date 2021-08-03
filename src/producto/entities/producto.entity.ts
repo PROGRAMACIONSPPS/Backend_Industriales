7ffb61138a55c994fc174d43967f9033f80c4b23
pedrodiaz0487 <pedrodiaz0487@gmail.com>
2021-08-03 15:26:17 -0300

    tabla producto para completar

--- src/producto/entities/producto.entity.ts
+++ src/producto/entities/producto.entity.ts
@@ -1,1 +1,93 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
 export class ProductoEntity {}
+
+/**
+ * Tabla que contiene las opciones de sexo (masculino, femenino,...)
+ */
+ @Entity()
+ export class  Producto {
+ 
+     @PrimaryGeneratedColumn()
+     id_producto: number;
+         
+    @Column({
+        type: "int",
+        nullable: false,
+        unique: true
+    })
+    codigo_barra: number;
+
+    @Column({
+        type: "varchar",
+        length: 255,
+        nullable: false
+    })
+    descripcion: string;
+
+    @Column({ 
+        type: "decimal",
+        precision: 8,
+        scale: 2,
+        default:0,
+        nullable: true
+    })
+    precio_unidad: number;
+
+    @Column({ 
+        type: "decimal",
+        precision: 8,
+        scale: 2,
+        default:0,
+        nullable: true
+    })
+    precio_kilo: number;
+
+    @Column({ 
+        type: "decimal",
+        precision: 8,
+        scale: 4,
+        default:0,
+        nullable: true
+    })
+    stock: number;
+
+    @Column({ 
+        type: "decimal",
+        precision: 8,
+        scale: 4,
+        default:0,
+        nullable: true
+    })
+    stock_minimo: number;
+
+    @Column({
+        type: "date",
+        nullable: true
+    })
+    vencimiento: Date;  
+
+    @Column({
+        type: "int",
+        nullable: false
+    })
+    taller_id: number;
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
+
+    
